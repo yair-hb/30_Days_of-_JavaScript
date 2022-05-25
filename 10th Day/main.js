5988ccbebfd7b19e790cb1504f556e31b8b29df5
yair-hb <aythb@hotmail.com>
2022-05-25 12:38:57 -0500

    comenzamos con el dia 10 del curso de javascript

---
+++ 10th Day/main.js
@@ -0,0 +1,20 @@
+console.log('DIA 10 DEL CURSO DE JAVASCRIPT')
+
+// LOS SETS SON  UNA COLECION DE ELEMENTOS. SOLO PUEDEN CONTENER ELEMENTOS UNICOS
+
+const companies = new Set()
+console.log(companies)
+
+const languages = [
+    'English',
+    'Finnish',
+    'English',
+    'French',
+    'Spanish',
+    'English',
+    'French'
+]
+
+const setOfLanguages = new Set(languages)
+console.log(languages)
+console.log(setOfLanguages)
