cabb5d1410f05908bd1a0b0dbe93ebe8d900b178
yair-hb <aythb@hotmail.com>
2022-06-08 21:09:04 -0500

    se trabaja en los ejercicios del dia 11 del curso de js

--- 11th Day/main.js
+++ 11th Day/main.js
@@ -131,11 +131,240 @@ const persona = {
     languages:['Amharic','English','Suomi(Finish)']
 }
 
-const getPersonInfo = obj => {
+const getPersonalData = obj => {
     const skills = obj.skills
     const formattedSkills = skills.slice(0,-1).join(', ')
     const languages = obj.languages
-    const formattedLanguages = languages.slice(0,-1).join(', ')
+    const formattedLanguajes = languages.slice(0,-1).join(', ')
+
+    personData = `${obj.firstName} ${obj.lastName} vive en ${obj.country}. el tiene ${obj.age} años, el es un ${obj.job} y enseña sobre ${formattedSkills} y sobre ${skills[skills.length -1]} tambien habla en ${formattedLanguajes} y un poco de ${languages[2]}`
+
+    return personData
 }
 
+console.log(getPersonalData(persona))
 
+
+
+// -------------------------------------
+// object parameter with destructuring
+
+
+const calculatePerimeter2 = ({width, height}) => {
+    return 2* (width+ height)
+}
+
+console.log(calculatePerimeter2(rect))
+
+
+//----------------------------------------
+
+const getPersonData = ({
+    firstName,
+    lastName,
+    age,
+    country,
+    job,
+    skills,
+    languages
+}) => {
+    const formattedSkills = skills.slice(0,-1).join(', ')
+    const formattedLanguajes = languages.slice(0,-1).join(', ')
+
+    personData = `${firstName} ${lastName} vive en ${country}. El tiene ${age} de edad y se dedica a ${job}, enseña sobre ${formattedSkills} y tambien sobre ${skills[skills.length -1]}. El habla ${formattedLanguajes} y un poco de ${languages[2]}`
+    return personData
+}
+
+console.log(getPersonData(persona))
+
+//------------------------------------------
+// destructurando objeto durante su iteracion 
+
+const todolist = [
+    {
+        task:"prepare js test",
+        time:'4/1/2020 8:30',
+        completed: true
+    },
+    {
+        task:"Give js test",
+        time:'4/1/2020 10:00',
+        completed: false
+    },
+    {
+        task:"Asses test result",
+        time:'4/1/2020 1:00',
+        completed: false
+    }
+]
+
+for (const{task, time, completed} of todolist){
+    console.log(task, time, completed)
+}
+
+//// --------------------------SPREAD O REST OPERADOR ----------------------
+// cuando destructuramos un array usando spread  el resto de los elementos se mueatran en un array y para eso usamos spread operador 
+// para separar los elementos de un array en otro array
+
+
+const numb = [1,2,3,4,5,6,7,8,9,10]
+let [num1, num2, num3,...rest2] = numb
+
+console.log(num1, num2, num3)
+console.log(rest2)
+
+//----------------------------
+
+const countries2 = ['Germany', 'France', 'Belgium', 'Finland', 'Sweden', 'Norway','Denmark', 'Iceland']
+
+let [gem, fra,,...nordicountries] = countries2
+
+console.log(gem)
+console.log(fra)
+console.log(nordicountries)
+
+//------------------------------ SPREAD OPERATOR TO COPY ARRAY
+
+
+const pares = [2,4,6,8,10]
+const numerosPares = [...pares]
+
+const impares = [1,3,5,7,9,11]
+const numerosImpares = [...impares]
+
+const todos = [...pares, ...impares]
+
+console.log(numerosPares)
+console.log(numerosImpares)
+console.log(todos)
+
+// ------------------------------- COPY AN OBJECT USING SPREAD OPERATOR 
+
+const usuario = {
+    name: 'Yair',
+    apellido: 'Hernandez',
+    age: 30,
+    title: 'Student',
+    country: 'Mexico',
+    city: 'CDMX'
+}
+
+console.log(usuario)
+
+const copyUser = {...usuario}
+console.log(copyUser)
+
+// --------------- SE PUEDE EDITAR LAS KEYS DEL OBJETO EN EL MISMO OPERADOR SPREAD
+
+const copy2 = {...usuario, title:"Programador"}
+console.log(copy2)
+
+//------------------SPREAD OPERATORS CON FUNCION FLECHA
+
+// siempre que usemos una funcion flecha que tome cualquier numero de argumentos usados como sprread operator los arggumentos pasados seran cambiados a un array
+
+const sumAllNums = (...args) => {
+    console.log(args)
+}
+
+sumAllNums(1,2,3,4,5,6,7,8,9)
+// el resultado mostrado sera en un array
+
+const sumAllNums2 = (...args) => {
+    let sum = 0
+    for (const num of args){
+    sum += num
+    }
+    return sum
+}
+
+console.log(sumAllNums2(1,2,3,4,5))
+
+///-------------------------------------------EJERCICIOS DEL DIA 11 DEL CURSO DE JS
+console.log('((((((((((((((Ejercicios del dia 11 del curso de js))))))))))))))')
+
+const constantes1 = [2.72, 3.14, 9.81, 37, 100]
+
+let [e1,pi1, gravedad1, hbt, boilingT] = constantes1
+console.log(e1, pi1, gravedad1, hbt, boilingT)
+
+//---------------------------------
+
+const constantes2 = ['Finland', 'Estonia', 'Sweden', 'Denmark', 'Norway']
+
+let [fin, est, swe, den, nor] = constantes2
+
+console.log(fin, est, swe, den, nor)
+
+//---------------------------------
+
+const rectangle1 = {
+    width3:20,
+    height3:10,
+    area3:200,
+    perimeter3:60
+}
+
+let {width3, height3, area3, perimeter3} = rectangle1
+
+console.log(width3, height3, area3, perimeter3)
+
+//----------------------------------
+
+const users = [
+    {
+      name:'Brook',
+      scores:75,
+      skills:['HTM', 'CSS', 'JS'],
+      age:16
+    },
+    {
+      name:'Alex',
+      scores:80,
+      skills:['HTM', 'CSS', 'JS'],
+      age:18
+    },
+    {
+      name:'David',
+      scores:75,
+      skills:['HTM', 'CSS'],
+      age:22
+    },
+    {
+      name:'John',
+      scores:85,
+      skills:['HTML'],
+      age:25
+    },
+    {
+      name:'Sara',
+      scores:95,
+      skills:['HTM', 'CSS', 'JS'],
+      age: 26
+    },
+    {
+      name:'Martha',
+      scores:80,
+      skills:['HTM', 'CSS', 'JS'],
+      age:18
+    },
+    {
+      name:'Thomas',
+      scores:90,
+      skills:['HTM', 'CSS', 'JS'],
+      age:20
+    }
+]
+
+for (const {name, scores, skills, age} of users){
+    console.log(name, scores, skills, age)
+}
+
+//---------------------------------
+console.log('--------------------------')
+
+const obt1 = {}
+for (const {name, skills} of users){
+    console.log(name, skills.length ),{}
+    
+}
